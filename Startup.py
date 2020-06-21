7222f5c3389c6b30272543c26895f64919c0f92c
Connor <hessconnor41@gmail.com>
2020-06-21 18:06:07 +0200

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -16,23 +16,32 @@ from tkinter import ttk
 import requests
 
 
+__author__ = 'Connor Hess'
+__copyright__ = 'Copyright (C) 2020, Connor Hess'
+__credits__ = ['Connor Hess']
+__license__ = 'The MIT License (MIT)'
+__version__ = (0.75)
+__maintainer__ = 'Connor Hess'
+__email__ = 'hessconnor41@gmail.com'
+__status__ = 'Alpha'
+
+
 
 def Update():
     pass
 
 def Update_manager():
-    requests.get('https://raw.githubusercontent.com/vsantiago113/Tkinter-MyTestApp/master/version.txt')
-    f1 = open(r"C:\Users\conno\OneDrive\Documents\GitHub\EasyPOS\version.txt", "r")
-    print(f1.read())
-    f2 = open("version.txt", "r")
-    print(f2.read())
-    if float(f1.read()) > float(f2.read()):
-        MsgBox = messagebox.askquestion ('Warning','Are you sure you want to Delete All\nThis will delete everything and can not be undone',icon = 'warning')
-        if MsgBox == 'yes':
-            Update()
-        else:
-            import login
-            login.Login_Page()
+    response = requests.get('https://github.com/connorhess/EasyPOS/blob/master/version.txt')
+    print(response)
+    data = response.text
+    print(data)
+##    if float(data) > __version__:
+##        MsgBox = messagebox.askquestion ('Warning','Are you sure you want to Delete All\nThis will delete everything and can not be undone',icon = 'warning')
+##        if MsgBox == 'yes':
+##            Update()
+##        else:
+##            import login
+##            login.Login_Page()
 
 
 
