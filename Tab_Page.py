b66d9c85328ad5ef28515c2f75549daa2811ca1a
Connor <hessconnor41@gmail.com>
2020-06-22 13:25:31 +0200

    update

--- Tab_Page.py
+++ Tab_Page.py
@@ -1,3 +1,38 @@
+import sqlite3
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+
+import time
+from datetime import timedelta
+import datetime
+from idle_time import IdleMonitor
+
+import random
+
+from prettytable import PrettyTable
+
+from functools import partial
+import sys
+import os
+
+import Error
+
+import tkinter as tk
+from tkinter import ttk
+
+
+Page1 = Tk()
+Page1.title("Shop Database")
+Page1.configure(background="#BEBEBE")
+Page1.geometry("500x300+300+300")
+Page1.iconbitmap('Till.ico')
+##Page1.attributes("-topmost", True)
+
+Photo = PhotoImage(file="Till.png",size=(300,250))
+
+lable = Label(Page1, image=Photo, height=10, width=10).pack()
 
 
 
@@ -9,44 +44,10 @@
 
 
 
-
-
-##import sqlite3
-##
-##from tkinter import *
-##from tkinter import filedialog
-##from tkinter import messagebox
-##
-##import time
-##from datetime import timedelta
-##import datetime
-##from idle_time import IdleMonitor
-##
-##import random
-##
-##from prettytable import PrettyTable
-##
-##from functools import partial
-##import sys
-##import os
-##
-##import Error
-##
-##import tkinter as tk
-##from tkinter import ttk
-##
-##
-####Page1 = Tk()
-####Page1.title("Shop Database")
-####Page1.configure(background="#BEBEBE")
-####Page1.geometry("500x300+300+300")
-####Page1.iconbitmap('Till.ico')
-######Page1.attributes("-topmost", True)
-##
 ##import tkinter as tk
 ##from tk_html_widgets import HTMLLabel
 ##
-####<img src="Till.jpg" style="width:104px;height:142px;">
+##<img src="Till.jpg" style="width:104px;height:142px;">
 ##
 ##root = tk.Tk()
 ##html_label = HTMLLabel(root, html='''<img src="Till.jpg" style="width:104px;height:142px;">''')
