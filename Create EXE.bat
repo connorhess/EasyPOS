9400673e5072b0709b30850587c024819fa7dedc
Connor <hessconnor41@gmail.com>
2020-06-27 11:41:51 +0200

    update

--- Create EXE.bat
+++ Create EXE.bat
@@ -3,12 +3,6 @@ echo Starting.........
 
 pyinstaller -w -F login.py
 
-echo Done
-
-echo renaming
-
-rename C:\Users\conno\OneDrive\Desktop\Shop\dist\login.py "EasyPOS V0.1.1"
-
 echo ====================================
 echo ================Done================
 echo ====================================
